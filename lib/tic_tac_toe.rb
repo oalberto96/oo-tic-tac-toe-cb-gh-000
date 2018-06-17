aaf84d90b98a7c7ee78763b29bc405acce747f5a
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-17 22:59:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,4 +43,17 @@ class TicTacToe
     return false
   end
 
+  def turn(board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+
+
 end
