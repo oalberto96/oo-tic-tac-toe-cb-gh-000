f4052a6f46eb65d471c4312d50aa4efc47240502
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-17 23:01:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,5 +55,24 @@ class TicTacToe
     end
   end
 
+  def turn_count
+    count = 0
+    @board.each do |square|
+      if square == "X" || square == "O"
+        count += 1
+      end
+    end
+    return count
+  end
+
+  def current_player
+    if turn_count(board) % 2 == 0
+      return "X"
+    else
+      return "O"
+    end
+  end
+
+
 
 end
