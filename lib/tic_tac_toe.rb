7b136ae65692cb2a3dd608ba44e59001d973f63d
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-17 22:58:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if index.between?(0,8) && position_taken?(@board, index) != true
+    if index.between?(0,8) && position_taken?(index) != true
       return true
     end
     return false
